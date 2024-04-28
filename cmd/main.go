a66c86e0f026f468b996a3d3b45c99fbe13a3c67
Florian Hübner <fhuebner@posteo.de>
2024-04-28 17:36:48 +0200

    example application

---
+++ cmd/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/nothub/semver"
+)
+
+const usage string = `semver - utilities for semantic versioning
+
+Options:
+    -h, -help    Show help
+
+Commands:
+    next - Bump to the next version
+    Usage: semver [opts...] next (major|minor|patch) <version>
+
+    strip - Remove pre-release or build metadata
+    Usage: semver [opts...] strip (all|pre|build) <version>
+`
+
+func main() {
+	log.SetFlags(0)
+	flag.Usage = func() {
+		log.Print(usage)
+	}
+	flag.Parse()
+
+	switch strings.ToLower(flag.Arg(0)) {
+	case "next":
+		next()
+	case "strip":
+		strip()
+	case "help":
+		fmt.Print(usage)
+		os.Exit(0)
+	default:
+		log.Print(usage)
+		os.Exit(1)
+	}
+}
+
+func next() {
+	if len(flag.Args()) < 3 {
+		log.Print(usage)
+		os.Exit(1)
+	}
+
+	ver, err := semver.Parse(flag.Arg(2))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	switch strings.ToLower(flag.Arg(1)) {
+	case "major":
+		i, err := ver.MajorInt()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		ver.Major = strconv.Itoa(i + 1)
+	case "minor":
+		i, err := ver.MinorInt()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		ver.Minor = strconv.Itoa(i + 1)
+	case "patch":
+		i, err := ver.PatchInt()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		ver.Patch = strconv.Itoa(i + 1)
+	default:
+		log.Print(usage)
+		os.Exit(1)
+	}
+
+	fmt.Print(ver.String())
+}
+
+func strip() {
+	if len(flag.Args()) < 3 {
+		log.Print(usage)
+		os.Exit(1)
+	}
+
+	ver, err := semver.Parse(flag.Arg(2))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	switch strings.ToLower(flag.Arg(1)) {
+	case "all":
+		ver.PreRelease = nil
+		ver.Build = nil
+	case "pre":
+		ver.PreRelease = nil
+	case "build":
+		ver.Build = nil
+	default:
+		log.Print(usage)
+		os.Exit(1)
+	}
+
+	fmt.Print(ver.String())
+}
